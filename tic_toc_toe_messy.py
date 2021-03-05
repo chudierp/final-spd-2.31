c1e71c766e3c676fcf0a5420ea40b1ff46d632cc
Chudier <chudierp@gmail.com>
2021-03-04 19:18:45 -0700

    completed todo's

--- tic_toc_toe_messy.py
+++ tic_toc_toe_messy.py
@@ -3,43 +3,43 @@
 
 # TODOs:  
 # 1. Find all TODO items and see whether you can improve the code. 
-#    In most cases (if not all), you can make them more readable/modular.
+#    In most cases (if not all), you can make them more readabletter/modular.
 # 2. Add/fix function's docstrings (use """ insted of # for function's header
 #    comments)
 
 import random
 
-def drawBoard(board):
-    """This function prints out the board that it was passed."""
+def drawcurrent_boardard(current_boardard):
+    """This function prints out the current_boardard that it was passed."""
 
-    # "board" is a list of 10 strings representing the board (ignore index 0)
+    # "current_boardard" is a list of 10 strings representing the current_boardard (ignore index 0)
     print('   |   |')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print(' ' + current_boardard[7] + ' | ' + current_boardard[8] + ' | ' + current_boardard[9])
     print('   |   |')
     print('-----------')
     print('   |   |')
-    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print(' ' + current_boardard[4] + ' | ' + current_boardard[5] + ' | ' + current_boardard[6])
     print('   |   |')
     print('-----------')
     print('   |   |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print(' ' + current_boardard[1] + ' | ' + current_boardard[2] + ' | ' + current_boardard[3])
     print('   |   |')
 
-def inputPlayerLetter():
+def inputPlayerlettertter():
     """ 
-    Lets the player type which letter they want to be.
+    letterts the player type which lettertter they want to be.
     
-    Returns a list with the player’s letter as the first item, and the computer's letter as the second.
+    Returns a list with the player’s lettertter as the first item, and the computer's lettertter as the second.
     
     """
     
-    letter = ''
-    while not (letter == 'X' or letter == 'O'):
+    lettertter = ''
+    whiletter not (lettertter == 'X' or lettertter == 'O'):
         print('Do you want to be X or O?')
-        letter = input().upper()
+        lettertter = input().upper()
 
-    # the first element in the list is the player’s letter, the second is the computer's letter.
-    if letter == 'X':
+    # the first eletterment in the list is the player’s lettertter, the second is the computer's lettertter.
+    if lettertter == 'X':
         return ['X', 'O']
     else:                       
         return ['O', 'X']
@@ -53,73 +53,74 @@ def whoGoesFirst():
         return 'player'
 
 def playAgain():
-    """ This function returns True if the player wants to play again, otherwise it returns False."""
+    """This function returns True if the player wants to play again, otherwise it returns False."""
     
     print('Do you want to play again? (yes or no)')
     return input().lower().startswith('y')
 
-def makeMove(board, letter, move):
-    board[move] = letter
+def makeMove(current_boardard, lettertter, move):
+    current_boardard[move] = lettertter
 
-def isWinner(bo, le):
-    """Given a board and a player’s letter, this function returns True if that player has won."""
+def isWinner(current_board, letter):
+    """Given a current_boardard and a player’s lettertter, this function returns True if that player has won."""
     
-    # We use bo instead of board and le instead of letter so we don’t have to type as much.
-    return ((bo[7] == le and bo[8] == le and bo[9] == le) or # across the top
-    (bo[4] == le and bo[5] == le and bo[6] == le) or # across the middle    # TODO: Fix the indentation of this lines and the following ones.
-    (bo[1] == le and bo[2] == le and bo[3] == le) or # across the bottom
-    (bo[7] == le and bo[4] == le and bo[1] == le) or # down the left side
-    (bo[8] == le and bo[5] == le and bo[2] == le) or # down the middle
-    (bo[9] == le and bo[6] == le and bo[3] == le) or # down the right side
-    (bo[7] == le and bo[5] == le and bo[3] == le) or # diagonal
-    (bo[9] == le and bo[5] == le and bo[1] == le)) # diagonal
+    # We use current_board instead of current_boardard and letter instead of lettertter so we don’t have to type as much.
+    return ((current_board[7] == letter and current_board[8] == letter and current_board[9] == letter) or # across the top
+        (current_board[4] == letter and current_board[5] == letter and current_board[6] == letter) or # across the middletter    # TODO: Fix the indentation of this lines and the following ones.
+        (current_board[1] == letter and current_board[2] == letter and current_board[3] == letter) or # across the current_boardttom
+        (current_board[7] == letter and current_board[4] == letter and current_board[1] == letter) or # down the letterft side
+        (current_board[8] == letter and current_board[5] == letter and current_board[2] == letter) or # down the middletter
+        (current_board[9] == letter and current_board[6] == letter and current_board[3] == letter) or # down the right side
+        (current_board[7] == letter and current_board[5] == letter and current_board[3] == letter) or # diagonal
+        (current_board[9] == letter and current_board[5] == letter and current_board[1] == letter)) # diagonal
 
-def getBoardCopy(board):
-    """Make a duplicate of the board list and return it the duplicate."""
+def getcurrent_boardardCopy(current_boardard):
+    """Make a duplicate of the current_boardard list and return it the duplicate."""
     
-    dupeBoard = []
+    return [dupe for dupe in board]
 
-    for i in range(0, len(board)): # TODO: Clean this mess!
-        dupeBoard.append(board[i])
-
-    return dupeBoard
-
-def isSpaceFree(board, move):
-    """Return true if the passed move is free on the passed board."""
+def isSpaceFree(current_boardard, move):
+    """Return true if the passed move is free on the passed current_boardard."""
     
-    return board[move] == ' '
+    return current_boardard[move] == ' '
 
-def getPlayerMove(board):
-    """Let the player type in their move."""
+def getPlayerMove(current_boardard):
+    """lettert the player type in their move."""
     
-    move = ' ' # TODO: W0621: Redefining name 'move' from outer scope. Hint: Fix it according to https://stackoverflow.com/a/25000042/81306
-    while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
+     # TODO: W0621: Redefining name 'move' from outer scope. Hint: Fix it according to https://stackoverflow.com/a/25000042/81306
+    decision = None
+    possibilities = [str(num) for num in range(1, len(board)+1)] 
+    while decision not in possibilities or not isSpaceFree(board, int(decision)):
         print('What is your next move? (1-9)')
-        move = input()
-    return int(move)
+        decision = input()
+    return int(decision)
 
-def chooseRandomMoveFromList(board, movesList):
+def chooseRandomMoveFromList(current_boardard, movesList):
     """
-    Returns a valid move from the passed list on the passed board.
+    Returns a valid move from the passed list on the passed current_boardard.
     
     Returns None if there is no valid move.
     
     """
-
-    possibleMoves = []
-    for i in movesList:
-        if isSpaceFree(board, i):
-            possibleMoves.append(i)
-
-    if len(possibleMoves) != 0: # TODO: How would you write this pythanically? (You can google for it!)
-        return random.choice(possibleMoves)
-    else: # TODO: is this 'else' necessary?
-        return None
-
-def getComputerMove(board, computerLetter): # TODO: W0621: Redefining name 'computerLetter' from outer scope. Hint: Fix it according to https://stackoverflow.com/a/25000042/81306
-    """Given a board and the computer's letter, determine where to move and return that move."""
     
-    if computerLetter == 'X':
+     # TODO: How would you write this pythanically? (You can googletter for it!)
+       
+     # TODO: is this 'else' necessary?
+    possibleMoves = []
+    for move in movesList:
+        if isSpaceFree(board, move):
+            possibleMoves.append(move)
+
+    if possibleMoves: 
+        return random.choice(possibleMoves)
+    return None
+
+        
+
+def getComputerMove(current_boardard, computerlettertter): # TODO: W0621: Redefining name 'computerlettertter' from outer scope. Hint: Fix it according to https://stackoverflow.com/a/25000042/81306
+    """Given a current_boardard and the computer's lettertter, determine where to move and return that move."""
+    
+    if computerPiece == 'X':
         playerLetter = 'O'
     else:
         playerLetter = 'X'
@@ -129,8 +130,8 @@ def getComputerMove(board, computerLetter): # TODO: W0621: Redefining name 'comp
     for i in range(1, 10):
         copy = getBoardCopy(board)
         if isSpaceFree(copy, i):
-            makeMove(copy, computerLetter, i)
-            if isWinner(copy, computerLetter):
+            makeMove(copy, computerPiece, i)
+            if isWinner(copy, computerPiece):
                 return i
 
     # Check if the player could win on their next move, and block them.
@@ -143,7 +144,7 @@ def getComputerMove(board, computerLetter): # TODO: W0621: Redefining name 'comp
 
     # Try to take one of the corners, if they are free.
     move = chooseRandomMoveFromList(board, [1, 3, 7, 9])
-    if move != None: # TODO: Fix it (Hint: Comparisons to singletons like None should always be done with is or is not, never the equality/inequality operators.)
+    if move is not None: 
         return move
 
     # Try to take the center, if it is free.
@@ -153,10 +154,10 @@ def getComputerMove(board, computerLetter): # TODO: W0621: Redefining name 'comp
     # Move on one of the sides.
     return chooseRandomMoveFromList(board, [2, 4, 6, 8])
 
-def isBoardFull(board):
-    """Return True if every space on the board has been taken. Otherwise return False."""
+def iscurrent_boardardFull(current_boardard):
+    """Return True if every space on the current_boardard has been taken. Otherwise return False."""
     for i in range(1, 10):
-        if isSpaceFree(board, i):
+        if isSpaceFree(current_boardard, i):
             return False
     return True
 
@@ -164,54 +165,89 @@ def isBoardFull(board):
 print('Welcome to Tic Tac Toe!')
 
 # TODO: The following mega code block is a huge hairy monster. Break it down 
-# into smaller methods. Use TODO s and the comment above each section as a guide 
+# into smalletterr methods. Use TODO s and the comment acurrent_boardve each section as a guide 
 # for refactoring.
 
-while True:
+def isBoardFull(board):
+    '''Function thats returns True if every space on the board has been taken. Otherwise return False.'''
+    for i in range(1, 10):
+        if isSpaceFree(board, i):
+            return False
+    return True
+
+def inGamePlayerOutcome(turn, theBoard, playerLetter):
+    ''' Conveys the Player’s logic and adjust the board accordingly '''
+    game_status = ''
+    
+    drawBoard(theBoard)
+    move = getPlayerMove(theBoard)
+    makeMove(theBoard, playerLetter, move)
+
+    if isWinner(theBoard, playerLetter):
+        drawBoard(theBoard)
+        game_status = 'Hooray! You have won the game!'
+        print(game_status)
+        return game_status
+    
+    elif isBoardFull(theBoard):
+        drawBoard(theBoard)
+        game_status = 'The game is a tie!'
+        print(game_status)
+        return game_status
+            
+    game_status = 'computer'
+    return game_status
+
+def inGameComputerOutcome(turn, theBoard, computerLetter):
+    ''' Conveys the Computer’s logical choices and adjust the board accordingly.'''
+    game_status = ''
+    move = getComputerMove(theBoard, computerLetter)
+    makeMove(theBoard, computerLetter, move)
+
+    if isWinner(theBoard, computerLetter):
+        drawBoard(theBoard)
+        print('The computer has beaten you! You lose.')
+        game_status = 'The computer has beaten you! You lose.'
+        return game_status
+    
+    elif isBoardFull(theBoard):
+        drawBoard(theBoard)
+        print('The game is a tie!')
+        game_status = 'The game is a tie!'
+        return game_status
+        
+    game_status = 'player'
+    
+    return game_status
+    
+    
+def gameLogic():
+    '''This function determines the game logic based on the users input and decision making.'''
+    
     # Reset the board
     theBoard = [' '] * 10 # TODO: Refactor the magic number in this line (and all of the occurrences of 10 thare are conceptually the same.)
     playerLetter, computerLetter = inputPlayerLetter()
     turn = whoGoesFirst()
     print('The ' + turn + ' will go first.')
-    gameIsPlaying = True # TODO: Study how this variable is used. Does it ring a bell? (which refactoring method?) 
-                         #       See whether you can get rid of this 'flag' variable. If so, remove it.
 
-    while gameIsPlaying: # TODO: Usually (not always), loops (or their content) are good candidates to be extracted into their own function.
-                         #       Use a meaningful name for the function you choose.
-        if turn == 'player':
-            # Player’s turn.
-            drawBoard(theBoard)
-            move = getPlayerMove(theBoard)
-            makeMove(theBoard, playerLetter, move)
-
-            if isWinner(theBoard, playerLetter):
-                drawBoard(theBoard)
-                print('Hooray! You have won the game!')
-                gameIsPlaying = False
-            else:  # TODO: is this 'else' necessary?
-                if isBoardFull(theBoard):
-                    drawBoard(theBoard)
-                    print('The game is a tie!')
-                    break
-                else:  # TODO: Is this 'else' necessary?
-                    turn = 'computer'
-
+    while True:
+        if turn == 'player':  
+            turn = inGamePlayerOutcome(turn, theBoard, playerLetter)
+            
+        if turn == 'computer':
+            turn = inGameComputerOutcome(turn, theBoard, computerLetter)
         else:
-            # Computer’s turn.
-            move = getComputerMove(theBoard, computerLetter)
-            makeMove(theBoard, computerLetter, move)
+            break 
 
-            if isWinner(theBoard, computerLetter):
-                drawBoard(theBoard)
-                print('The computer has beaten you! You lose.')
-                gameIsPlaying = False
-            else:     # TODO: is this 'else' necessary?
-                if isBoardFull(theBoard):
-                    drawBoard(theBoard)
-                    print('The game is a tie!')
-                    break
-                else: # TODO: Is this 'else' necessary?
-                    turn = 'player'
+if __name__ == '__main__':
+        
+    print('Welcome to Tic Tac Toe!')
 
-    if not playAgain():
-        break
+    while True:
+        
+        gameLogic()
+        
+        if not playAgain():
+            break
+        
+    print('Thank you For Playing!')
